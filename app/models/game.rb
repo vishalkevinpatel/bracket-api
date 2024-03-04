308fe068bf9e5f549f62684553ce066dd5714713
Emily Jorgensen <emjorg10@gmail.com>
2024-03-04 11:54:11 -0600

    game relationships

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  belongs_to :home_team
-  belongs_to :away_team
-  belongs_to :winner_team
+  belongs_to :home_team, class_name: "Team"
+  belongs_to :away_team, class_name: "Team"
+  belongs_to :winner_team, class_name: "Team", optional: true
 end
