919e172bba89f26ff6ac34dd685de7b06ceda32e
Emily Jorgensen <emjorg10@gmail.com>
2024-03-04 11:55:03 -0600

    team relationships

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,7 @@
 class Team < ApplicationRecord
+  has_many :home_games, class_name: "Game", foreign_key: "home_team_id"
+  has_many :away_games, class_name: "Game", foreign_key: "away_team_id"
+  has_many :winner_games, class_name: "Game", foreign_key: "winner_team_id"
+  # has_many :matches, class_name: "Match", foreign_key: "pick_team_id"
+  # has_many :matches, class_name: "Match", foreign_key: "winner_team_id"
 end
